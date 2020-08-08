f384dd12e56caf6578ae136ae49746e498c98f61
Phil Lisak <phil.lisak@gmail.com>
2020-08-07 17:34:13 -0700

    Added array of objects for existing team members

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,15 +1,39 @@
 import React, { useState } from 'react'
 
-const Form = () => {
-    const [members, setMembers] = useState([{
+const team = [{
+    name: 'Malcolm Reynolds',
+    email: 'browncoats4lyfe@xian.ch',
+    role: 'Captain'
+},
+{
+    name: 'Hoban Washburn',
+    email: 'leafonthewind@wahoo.com',
+    role: 'Pilot'
+},
+{
+    name: 'Kaylee Frye',
+    email: 'keepflyin@serenity.ship',
+    role: 'Mechanic, Cutie-Pie'
+}];
+
+const Form = props => {
+    const [members, setMembers] = useState({
         name: '',
         email: '',
         role: ''
-    }])
+    })
 
 
     return (
         <div>
+            <form>
+                <label htmlFor='name'>Name</label>
+                <input 
+                id='name'
+                type='text'
+                placeholder='Enter your name'
+                />
+            </form>
             
         </div>
     )
