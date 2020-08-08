d5df0327c3a74de1a7f3417e2450aff49d5939ad
Phil Lisak <phil.lisak@gmail.com>
2020-08-07 17:39:36 -0700

    Completed form markup

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -27,12 +27,28 @@ const Form = props => {
     return (
         <div>
             <form>
-                <label htmlFor='name'>Name</label>
+                <label htmlFor='name'>Name: </label>
                 <input 
                 id='name'
                 type='text'
                 placeholder='Enter your name'
                 />
+                <br />
+                <label htmlFor='email'>Email: </label>
+                <input 
+                id='email'
+                type='email'
+                placeholder='Enter your email'
+                /> 
+                <br />
+                <label htmlFor='role'>Job Description: </label>
+                <input
+                id='role'
+                type='text'
+                placeholder='What do you do here?'
+                />
+                <br />
+                <button type='submit'>Join Our Crew</button>
             </form>
             
         </div>
