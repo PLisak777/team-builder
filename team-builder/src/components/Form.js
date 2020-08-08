96682cb26f0eba82b5cf0221b33fbaef94f26000
Phil Lisak <phil.lisak@gmail.com>
2020-08-07 17:19:34 -0700

    Created Form.js, imported useState and created initial hooks for members, setMembers

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+    const [members, setMembers] = useState([{
+        name: '',
+        email: '',
+        role: ''
+    }])
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Form
+
+
